31d4d450fa99f2b52a2b402e2e54bb64fc786aa4
vinylguy <vinylguy@fullsail.edu>
2012-04-26 18:28:36 -0700

    just saving some minor code tweaks, cleanup of unused comments, and bug chasing

--- nbaker_lib.js
+++ nbaker_lib.js
@@ -18,12 +18,13 @@ return min;
 var j = new Array();
 j = [7, 23, 4, 90, -1, 44, 93];
 
+//Logs the output of the smallest value in a given array
 console.log(minValue(j));
 
 //Find the value of numbers in array, even if some are not numbers.
 var totValue = function (array) {
 	tot = array[0];
-	for (var i=0; i<array.length; i++) {
+	for (i=0; i<array.length; i++) {
 		if (!isNaN(parseInt(array[i]))) {
 			tot += array[i];
 		};
@@ -35,24 +36,27 @@ return tot;
 var k = new Array();
 k = [7, "a", 4, 90, "cat", 44, 93];
 
+//Logs the value of the numbers in the array
 console.log(totValue(k));
 
 //Validate an email.
+
 function validateEmail (email) {
 	var x = email
 	var atPos = x.indexOf("@");
 	var dotPos = x.lastIndexOf(".");
 		if (atPos<1 || dotPos<atPos+2 || dotPos+2>x.length) {
-				/*if (validateEmail( true ))*/ 
 				console.log("Good Email");
 		} else { 
 				console.log("Bad Email");
 		};
 };
 
+//Calls the function "validateEmail"
 validateEmail("vinylguy@gmail.com");
 
 //Find the amount of days between 2 dates.
+
 Date.daysBetween = function ( date1, date2 ) {
 	var oneDay = 1000 * 60 * 60 * 24;
 	var date1_ms = date1.getTime();
@@ -65,6 +69,7 @@ var startDate = new Date(2012, 3, 20);
 var endDate = new Date(startDate.getFullYear() + startDate.getMonth(), startDate.getDate());
 var today = new Date();
 
+//Logs the amount of days between two dates.
 console.log( "Days since" + endDate.toLocaleDateString() + "; "
 			+ Date.daysBetween(startDate, today)
 			);
@@ -83,6 +88,7 @@ function isValidNumber (number) {
 
 };
 
+//Determines if a number is valid in 123-456-7891 format
 isValidNumber();
 
 //Change the decimal place on a given number, to a given amount of decimal places.
@@ -94,27 +100,5 @@ function changeDecimal(input) {
 	return output
 };
 
+//Changes the decimal point of a given number two places.
 changeDecimal(2.1);
-
-//String functions
-/*
-var isURL = function () {
-
-	return { };
-};
-
-var isValidPhoneNumber = function () {
-
-	return { };
-};
-
-//Number functions
-
-
-
-
-
-//Array object
-
-[{a:3},{a:2},{a:1},{a:4}]
-*/
